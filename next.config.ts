95b3a08d7729c89caf857fe455839e4c0d45baf9
daviloops <91davidlopez@gmail.com>
2025-10-22 17:10:00 -0600

    fix: config svgr webpack

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,30 @@ const nextConfig: NextConfig = {
       },
     },
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
 };
 
 export default nextConfig;
