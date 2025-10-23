9ebbc26e91e1d127187b8d9f008b1544f233119f
daviloops <91davidlopez@gmail.com>
2025-10-23 16:53:36 -0600

    refactor: add font preloading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,6 +79,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="preload"
+          href="/fonts/Aleo-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Aleo-Bold.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Providers>
           {children}
