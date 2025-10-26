85c9641c7543379a9b00be1301064f5680504291
daviloops <91davidlopez@gmail.com>
2025-10-25 18:34:22 -0600

    feat: improved carousel

---
+++ src/app/components/Facilities/EmblaCarousel/index.tsx
@@ -0,0 +1,134 @@
+'use client';
+
+import React, { useCallback, useEffect, useRef } from 'react';
+import Image from 'next/image';
+import { EmblaCarouselType, EmblaEventType, EmblaOptionsType } from 'embla-carousel';
+import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay';
+
+import './embla.css';
+
+const TWEEN_FACTOR_BASE = 0.52;
+
+const numberWithinRange = (number: number, min: number, max: number): number =>
+  Math.min(Math.max(number, min), max);
+
+type Slide = {
+  imageSrc: string;
+  altText: string;
+  sizes: string;
+  width: number;
+  height: number;
+};
+
+type PropType = {
+  slides: Slide[];
+  options?: EmblaOptionsType;
+};
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [
+    Autoplay({ playOnInit: true, delay: 4000 }),
+  ]);
+  const tweenFactor = useRef(0);
+  const tweenNodes = useRef<HTMLElement[]>([]);
+
+  const setTweenNodes = useCallback((emblaApi: EmblaCarouselType): void => {
+    tweenNodes.current = emblaApi.slideNodes().map((slideNode) => {
+      return slideNode.querySelector('.embla__slide__number') as HTMLElement;
+    });
+  }, []);
+
+  const setTweenFactor = useCallback((emblaApi: EmblaCarouselType) => {
+    tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length;
+  }, []);
+
+  const tweenScale = useCallback((emblaApi: EmblaCarouselType, eventName?: EmblaEventType) => {
+    const engine = emblaApi.internalEngine();
+    const scrollProgress = emblaApi.scrollProgress();
+    const slidesInView = emblaApi.slidesInView();
+    const isScrollEvent = eventName === 'scroll';
+
+    emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
+      let diffToTarget = scrollSnap - scrollProgress;
+      const slidesInSnap = engine.slideRegistry[snapIndex];
+
+      slidesInSnap.forEach((slideIndex) => {
+        if (isScrollEvent && !slidesInView.includes(slideIndex)) return;
+
+        if (engine.options.loop) {
+          engine.slideLooper.loopPoints.forEach((loopItem) => {
+            const target = loopItem.target();
+
+            if (slideIndex === loopItem.index && target !== 0) {
+              const sign = Math.sign(target);
+
+              if (sign === -1) {
+                diffToTarget = scrollSnap - (1 + scrollProgress);
+              }
+              if (sign === 1) {
+                diffToTarget = scrollSnap + (1 - scrollProgress);
+              }
+            }
+          });
+        }
+
+        const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor.current);
+        const scale = numberWithinRange(tweenValue, 0, 1).toString();
+        const tweenNode = tweenNodes.current[slideIndex];
+        tweenNode.style.transform = `scale(${scale})`;
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    setTweenNodes(emblaApi);
+    setTweenFactor(emblaApi);
+    tweenScale(emblaApi);
+
+    emblaApi
+      .on('reInit', setTweenNodes)
+      .on('reInit', setTweenFactor)
+      .on('reInit', tweenScale)
+      .on('scroll', tweenScale)
+      .on('slideFocus', tweenScale);
+  }, [emblaApi, tweenScale]);
+
+  // TODO: define 3 variables for carousel
+  // --slide-height: 19rem;
+  // --slide-spacing: 1rem;
+  // --slide-size: 55%;
+
+  return (
+    <div className="embla w-full">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map(({ imageSrc, altText, sizes, width, height }: Slide, index) => (
+            <div className="embla__slide flex-[0_0_55%] lg:flex-[0_0_35%]" key={index}>
+              <div className="embla__slide__number h-[243] sm:h-[276] md:h-[312] lg:h-[288] xl:h-[333] 2xl:h-[366]">
+                {/* TODO: maybe add biggest size to mobile w-[340] h-[255] */}
+                <div
+                  className={`relative aspect-${width}/${height} h-[243] w-[324] sm:h-[276] sm:w-[368] md:h-[312] md:w-[416] lg:h-[288] lg:w-[384] xl:h-[333] xl:w-[444] 2xl:h-[366] 2xl:w-[488]`}
+                >
+                  <Image
+                    className="embla__slide__img h-[243] object-cover sm:h-[276] md:h-[312] lg:h-[288] xl:h-[333] 2xl:h-[366]"
+                    src={imageSrc}
+                    alt={altText}
+                    sizes={sizes}
+                    fill
+                    loading="lazy"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { EmblaCarousel };
--- src/app/components/Facilities/index.tsx
+++ src/app/components/Facilities/index.tsx
@@ -1,92 +1,54 @@
-import Image from 'next/image';
+import { EmblaOptionsType } from 'embla-carousel';
 
-import { Section } from '../Section';
+import { EmblaCarousel } from './EmblaCarousel';
 
-// TODO: refactor
-// const facilityImages = [
-//   {
-//     src: "/images/facilities/palapa-con-mesas.webp",
-//     alt: "mesas bajo la palapa principal",
-//     width: 1920,
-//     height: 1280,
-//   },
-// ];
+const OPTIONS: EmblaOptionsType = { loop: true };
+const SLIDES = [
+  {
+    imageSrc: '/images/facilities/palapa-con-mesas.webp',
+    altText: 'mesas bajo la palapa principal',
+    sizes: '(max-width: 640px) 404px, (max-width: 768px) 450px, (max-width: 1024px) 500px, 666px',
+    width: 600, // forced to fit, real value is 720
+    height: 450, // forced to fit, real value is 480
+  },
+  {
+    imageSrc: '/images/facilities/area-privada.webp',
+    altText: 'area privada para comer y eventos',
+    sizes:
+      '(max-width: 640px) 324px, (max-width: 768px) 368px, (max-width: 1024px) 416px, (max-width: 1280px) 384px, (max-width: 1536px) 444px, 488px',
+    width: 600,
+    height: 450,
+  },
+  {
+    imageSrc: '/images/facilities/palapa-y-exterior.webp',
+    altText: 'mesas bajo la palapa principal y jardín exterior',
+    sizes:
+      '(max-width: 640px) 324px, (max-width: 768px) 368px, (max-width: 1024px) 416px, (max-width: 1280px) 384px, (max-width: 1536px) 444px, 488px',
+    width: 600,
+    height: 450,
+  },
+  {
+    imageSrc: '/images/facilities/area-descanso-con-techumbre.webp',
+    altText: 'area de descanso con sillas y decoraciones bajo un techumbre de madera',
+    sizes:
+      '(max-width: 640px) 324px, (max-width: 768px) 368px, (max-width: 1024px) 416px, (max-width: 1280px) 384px, (max-width: 1536px) 444px, 488px',
+    width: 600,
+    height: 450,
+  },
+  {
+    imageSrc: '/images/facilities/area-ninos.webp',
+    altText: 'area de niños con juegos',
+    sizes: '(max-width: 640px) 298px, (max-width: 768px) 332px, (max-width: 1024px) 368px, 492px',
+    width: 600,
+    height: 450, // forced to fit, real value is 543
+  },
+];
 
 const Facilities = () => {
   return (
-    <Section className="flex items-center justify-center bg-[#EDE4DF] pt-15 pb-19 sm:pt-16 sm:pb-20 md:pt-18 md:pb-21 lg:pt-25 lg:pb-30">
-      <div className="relative flex w-full flex-col overflow-x-hidden sm:max-w-[612] md:max-w-[693] lg:max-w-[960]">
-        <div className="carousel carousel-center rounded-box space-x-4 sm:space-x-5 md:space-x-6 lg:space-x-10">
-          <div id="item1" className="carousel-item">
-            <div className="relative aspect-1920/1280 h-[200] min-[500]:h-[269] sm:h-[300] md:h-[333] lg:h-[444]">
-              <Image
-                alt="mesas bajo la palapa principal"
-                src="/images/facilities/palapa-con-mesas.webp"
-                fill
-                sizes="(max-width: 640px) 404px, (max-width: 768px) 450px, (max-width: 1024px) 500px, 666px"
-                className="object-cover"
-                priority
-              />
-            </div>
-          </div>
-          <div id="item2" className="carousel-item">
-            <div className="relative aspect-1920/1440 h-[200] min-[500]:h-[269] sm:h-[300] md:h-[333] lg:h-[444]">
-              <Image
-                alt="mesas bajo la palapa principal y jardín exterior"
-                src="/images/facilities/palapa-y-exterior.webp"
-                fill
-                sizes="(max-width: 640px) 360px, (max-width: 768px) 400px, (max-width: 1024px) 444px, 592px"
-                className="object-cover"
-                loading="lazy"
-              />
-            </div>
-          </div>
-          <div id="item3" className="carousel-item">
-            <div className="relative aspect-4608/3456 h-[200] min-[500]:h-[269] sm:h-[300] md:h-[333] lg:h-[444]">
-              <Image
-                alt="area privada para comer y eventos"
-                src="/images/facilities/area-privada.webp"
-                fill
-                sizes="(max-width: 640px) 360px, (max-width: 768px) 400px, (max-width: 1024px) 444px, 592px"
-                className="object-cover"
-                loading="lazy"
-              />
-            </div>
-          </div>
-          <div id="item4" className="carousel-item">
-            <div className="relative aspect-1920/1440 h-[200] min-[500]:h-[269] sm:h-[300] md:h-[333] lg:h-[444]">
-              <Image
-                alt="area de descanso con sillas y decoraciones bajo un techumbre de madera"
-                src="/images/facilities/area-descanso-con-techumbre.webp"
-                fill
-                sizes="(max-width: 640px) 360px, (max-width: 768px) 400px, (max-width: 1024px) 444px, 592px"
-                className="object-cover"
-                loading="lazy"
-              />
-            </div>
-          </div>
-          <div id="item5" className="carousel-item">
-            <div className="relative aspect-3816/3456 h-[200] min-[500]:h-[269] sm:h-[300] md:h-[333] lg:h-[444]">
-              <Image
-                alt="area de niños con juegos"
-                src="/images/facilities/area-ninos.webp"
-                fill
-                sizes="(max-width: 640px) 298px, (max-width: 768px) 332px, (max-width: 1024px) 368px, 492px"
-                className="object-cover"
-                loading="lazy"
-              />
-            </div>
-          </div>
-        </div>
-        {/* <div className="flex w-full justify-center gap-2 py-2">
-          <a href="#item1" className="btn btn-xs">1</a>
-          <a href="#item2" className="btn btn-xs">2</a>
-          <a href="#item3" className="btn btn-xs">3</a>
-          <a href="#item4" className="btn btn-xs">4</a>
-          <a href="#item5" className="btn btn-xs">5</a>
-        </div> */}
-      </div>
-    </Section>
+    <section className="flex items-center justify-center bg-[#EDE4DF] pt-15 pb-19 sm:pt-16 sm:pb-20 md:pt-18 md:pb-21 lg:pt-25 lg:pb-30">
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+    </section>
   );
 };
 
