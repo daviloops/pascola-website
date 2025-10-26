35d944259edf4cacb46e2a820fb9b8f528e0929b
daviloops <91davidlopez@gmail.com>
2025-10-25 22:25:05 -0600

    style: increase menu button font size

--- src/app/components/Hero/MenuButton/index.tsx
+++ src/app/components/Hero/MenuButton/index.tsx
@@ -6,7 +6,7 @@ const MenuButton = ({ className, children, ...props }: AnchorHTMLAttributes<HTML
     <div className="min-w-[160]">
       <a
         className={clsx(
-          'btn btn-neutral btn-outline w-full rounded-2xl border bg-[#D9D9D9] text-[24px] font-bold text-black hover:bg-black hover:text-[#D9D9D9] active:bg-[#272727] active:text-[#D9D9D9]',
+          'btn btn-neutral btn-outline w-full rounded-2xl border bg-[#D9D9D9] text-[18px] font-bold text-black hover:bg-black hover:text-[#D9D9D9] active:bg-[#272727] active:text-[#D9D9D9]',
           className
         )}
         {...props}
