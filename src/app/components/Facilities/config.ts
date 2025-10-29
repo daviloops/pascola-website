92f2771b96e1e340e58a43f5ccb24fa0b8975e8f
daviloops <91davidlopez@gmail.com>
2025-10-29 16:13:15 -0600

    chore: reduce slide time on carousels & change facilities image

--- src/app/components/Facilities/config.ts
+++ src/app/components/Facilities/config.ts
@@ -2,4 +2,4 @@ import type { EmblaOptionsType } from 'embla-carousel';
 import type { AutoplayOptionsType } from 'embla-carousel-autoplay';
 
 export const OPTIONS: EmblaOptionsType = { loop: true };
-export const AUTOPLAY_OPTIONS: AutoplayOptionsType = { delay: 4000 };
+export const AUTOPLAY_OPTIONS: AutoplayOptionsType = { delay: 3000 };
